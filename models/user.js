b81a97fdf4815886b59a07a6e678e50c7a3d2077
fernando <fernandoguerreroc65@gmail.com>
2017-05-15 18:44:38 -0500

    created model user whit pass encryp

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcrypt-nodejs')
+const crypto = require('crypto')
 
 const UserSchema = new Schema({
   email: {type: String, unique: true, lowecase: true},
@@ -11,12 +12,13 @@ const UserSchema = new Schema({
   lastLogin: Date
 })
 
+// se encripta la contraseña que ingresa el usuario
 UserSchema.pre('save', (next) => {
   let user = this
-  if (!user.isModificated('passwor')) return next()
+  if (!user.isModified('passwor')) return next()
 
   bcrypt.genSalt(10, (err, salt) => {
-    if (err) return next()
+    if (err) return next(err)
 
     bcrypt.hash(user.password, salt, null, (err, hash) => {
       if (err) return next(err)
@@ -26,3 +28,14 @@ UserSchema.pre('save', (next) => {
     })
   })
 })
+
+
+UserSchema.methods.gravatar = () => {
+  // si el usuario no ha ingresado un avatar se le asigna uno por default
+  if (!this.email) return 'https://gravatar.com/avatar/?s=200&d=retro'
+
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+  return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
+}
+
+module.exports = mongoose.model('User', UserSchema)
